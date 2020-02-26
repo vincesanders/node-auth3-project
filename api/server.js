c9deb7f6d75239deb058e76d5fb8877704d22406
Vincent Sanders <vincesanders@hotmail.com>
2020-02-26 15:25:44 -0600

    Added endpoints for auth and use router as well as basic scaffolding for each.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const authRouter = require('../auth/authRouter');
+const userRouter = require('../user/userRouter');
 
 const server = express();
 server.use(helmet(), express.json(), cors());
+
+server.use('api/auth', authRouter);
+server.use('api/users', userRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'It is alive!!!' });
