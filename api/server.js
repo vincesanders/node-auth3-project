8b2863596578d88f18d87c5fa83a38668d3a55ca
Vincent Sanders <vincesanders@hotmail.com>
2020-02-26 16:30:23 -0600

    Fixed some bugs with the checkRole middleware.

--- api/server.js
+++ api/server.js
@@ -4,12 +4,13 @@ const cors = require('cors');
 const authRouter = require('../auth/authRouter');
 const userRouter = require('../user/userRouter');
 const restricted = require('../utils/restricted');
+const checkRole = require('../utils/checkRole');
 
 const server = express();
 server.use(helmet(), express.json(), cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', restricted, userRouter);
+server.use('/api/users', restricted, checkRole('admin'), userRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'It is alive!!!' });
