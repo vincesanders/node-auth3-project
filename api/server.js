386e4c4c7ea94c3906e0af1446fcb31e95341f75
Vincent Sanders <vincesanders@hotmail.com>
2020-02-26 16:18:51 -0600

    Added middleware to users endpoint so it could only be accessed by users with a token.

--- api/server.js
+++ api/server.js
@@ -3,12 +3,13 @@ const helmet = require('helmet');
 const cors = require('cors');
 const authRouter = require('../auth/authRouter');
 const userRouter = require('../user/userRouter');
+const restricted = require('../utils/restricted');
 
 const server = express();
 server.use(helmet(), express.json(), cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', userRouter);
+server.use('/api/users', restricted, userRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'It is alive!!!' });
