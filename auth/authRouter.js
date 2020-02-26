2f942b45f78fc8b59c557a19323a3eb5583acb7d
Vincent Sanders <vincesanders@hotmail.com>
2020-02-26 16:04:34 -0600

    Added the register endpoint.

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const users = require('../user/userModel');
+const generateToken = require('../utils/generateToken');
+
+router.post('/register', (req, res) => {
+    const { username, password, role } = req.body;
+    let user = { username, password, role };
+    const hash = bcrypt.hashSync(user.password, 8); //higher in production
+    user.password = hash;
+
+    users.insert(user).then(addedUser => {
+        const token = generateToken(addedUser);
+        res.status(201).json({ user: addedUser, token });
+    }).catch(({ error, message, stack }) => {
+        console.log(`Error: ${error}\nMessage: ${message}\n Stack: ${stack}`)
+        res.status(500).json({ error, message, stack, errorMessage: 'Unable to register user.' })
+    });
+});
 
 module.exports = router;
-
