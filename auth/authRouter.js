4a874bcf85c3e533a8804b81da24860bfce3d812
Vincent Sanders <vincesanders@hotmail.com>
2020-02-26 15:34:20 -0600

    Added anticipated db functions the app would need to user model.

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const users = require('../user/userModel');
 
 module.exports = router;
 
--- user/userRouter.js
+++ user/userRouter.js
@@ -1,3 +1,4 @@
 const router = require('express').Router();
+const users = require('./userModel');
 
 module.exports = router;
